040acde1d3d11af6dfe62dad10c90cda547f9b3d
kamattson <kmattson@ecsteam.com>
2017-09-12 14:06:25 -0500

    using user:password

--- exporter.sh
+++ exporter.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
-ORGS=(
+
+
+USER=(
 "org1:xxxxxxxxxx"
 "org2:xxxxxxxxxx")
-HOST="http://your.grafana.host"
+
+USER=admin
+PASSWORD=ir8isypsg7dynjekdu5z
+
+HOST="172.28.106.6:3000"
 FILE_DIR=path/to/export
 
 fetch_fields() {
-    echo $(curl -sSL -f -k -H "Authorization: Bearer ${1}" "${HOST}/api/${2}" | jq -r "if type==\"array\" then .[] else . end| .${3}")
+    echo $(curl  "http://${USER}:${PASSWORD}@${HOST}/api/${1}" | jq -r "if type==\"array\" then .[] else . end| .${2}")
 }
 
 if [ ! -d "$FILE_DIR" ] ; then
@@ -20,7 +26,7 @@ for row in "${ORGS[@]}" ; do
 
     if [ ! -d "$DIR" ] ; then
     	mkdir -p "$DIR"
-	fi
+	  fi
 
 	if [ ! -d "$DIR/dashboards" ] ; then
 	    mkdir -p "$DIR/dashboards"
@@ -30,16 +36,16 @@ for row in "${ORGS[@]}" ; do
     	    mkdir -p "$DIR/datasources"
     	fi
 
-    for dash in $(fetch_fields $KEY 'search?query=&' 'uri'); do
+    for dash in $(fetch_fields 'search?query=&' 'uri'); do
         DB=$(echo ${dash}|sed 's,db/,,g').json
         echo $DB
-    	curl -f -k -H "Authorization: Bearer ${KEY}" "${HOST}/api/dashboards/${dash}" | jq '.dashboard.id = null' | jq '.overwrite = true' > "$DIR/dashboards/$DB"
+    	curl "http://${USER}:${PASSWORD}@${HOST}/api/dashboards/${dash}" | jq '.dashboard.id = null' | jq '.overwrite = true' > "$DIR/dashboards/$DB"
     done
 
 	for id in $(fetch_fields $KEY 'datasources' 'id'); do
         DS=$(echo $(fetch_fields $KEY "datasources/${id}" 'name')|sed 's/ /-/g').json
         echo $DS
-        curl -f -k -H "Authorization: Bearer ${KEY}" "${HOST}/api/datasources/${id}" | jq '.id = null' | jq '.orgId = null' > "$DIR/datasources/$DS"
+        curl "http://${USER}:${PASSWORD}@${HOST}/api/datasources/${id}" | jq '.id = null' | jq '.orgId = null' > "$DIR/datasources/$DS"
     done
 
 done
